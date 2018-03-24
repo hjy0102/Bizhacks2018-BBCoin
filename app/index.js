8ac03ded5d635bf05b5e0dfa73eb3d575dda0e83
David Park <david.park@paihealth.com>
2018-03-24 09:48:11 -0700

    fix npm start

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3200;
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function() {
+  console.log('local port number: ' + port);
 });
