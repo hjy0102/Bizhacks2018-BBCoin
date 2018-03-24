717cb7de3e809bf096e9f9440fe5b871ddf84316
David Park <david.park@paihealth.com>
2018-03-24 10:08:42 -0700

    add relative paths to css and images

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var path = require('path')
 var port = process.env.PORT || 3200;
+
+app.use(app.static(path.join(__dirname, 'images')));
+app.use(app.static(path.join(__dirname, 'stylesheets')));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -9,6 +12,18 @@ app.get('/', function(req, res) {
 
 app.get('/main.html', function(req, res) {
   res.sendFile(__dirname + '/main.html');
+});
+
+app.get('/confirmationPage.html', function(req, res) {
+  res.sendFile(__dirname + '/confirmationPage.html');
+});
+
+app.get('/payPage.html', function(req, res) {
+  res.sendFile(__dirname + '/payPage.html');
+});
+
+app.get('/reloadPage.html', function(req, res) {
+  res.sendFile(__dirname + '/reloadPage.html');
 });
 
 http.listen(port, function() {
