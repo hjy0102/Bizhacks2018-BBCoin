4e2ed6128fa170e8aa4531cddc8af46db065e701
David Park <david.park@paihealth.com>
2018-03-24 09:56:33 -0700

    testing index.js

--- app/index.js
+++ app/index.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/main.html', function(req, res) {
+  res.sendFile(__dirname + '/main.html');
+});
+
 http.listen(port, function() {
   console.log('local port number: ' + port);
 });
