33c7a4720fe1d2ba39a7c17146a27cf8d5ebd9aa
David Park <david.park@paihealth.com>
2018-03-24 11:54:02 -0700

    successfully deployed to heroku

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
 var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + './images'));
-app.use(express.static(__dirname + './stylesheets'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
+app.use('/javascripts', express.static(__dirname + '/javascripts'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -26,6 +26,6 @@ app.get('/reloadPage.html', function(req, res) {
   res.sendFile(__dirname + '/reloadPage.html');
 });
 
-http.listen(port, function() {
+app.listen(port, function() {
   console.log('local port number: ' + port);
 });
